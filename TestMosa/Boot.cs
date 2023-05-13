7225f3e296c3c8dade25ef1c92e9ab8e193744c1
Misha <mishakeys20@gmail.com>
2023-05-13 14:22:10 -0400

    Add mosa project for testing

---
+++ TestMosa/Boot.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.DeviceSystem;
+using Mosa.Kernel.x86;
+using Mosa.Runtime.Plug;
+
+namespace TestMosa
+{
+    public static class Boot
+    {
+        [Plug("Mosa.Runtime.StartUp::SetInitialMemory")]
+        public static void SetInitialMemory()
+        {
+            KernelMemory.SetInitialMemory(Address.GCInitialMemory, 0x01000000);
+        }
+
+        public static void Main()
+        {
+            #region Initialization
+
+            Kernel.Setup();
+            IDT.SetInterruptHandler(ProcessInterrupt);
+
+            #endregion
+
+            Program.Setup();
+
+            for (; ; )
+                Program.Loop();
+        }
+
+        public static void ProcessInterrupt(uint interrupt, uint errorCode)
+        {
+            if (interrupt >= 0x20 && interrupt < 0x30)
+                HAL.ProcessInterrupt((byte)(interrupt - 0x20));
+        }
+    }
+}
---
+++ TestMosa/Program.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using System;
+
+namespace TestMosa
+{
+    public static class Program
+    {
+        public static void Setup()
+        {
+            Console.ResetColor();
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Loop()
+        { }
+    }
+}
